194fb59833f5772b35df39feed6bb4d8b52481b4
shinesoleil <hao_pan@outlook.com>
2015-11-21 17:26:25 +0800

    first practice done

--- src/calculate-first.js
+++ src/calculate-first.js
@@ -1,3 +1,19 @@
+function isEvenIndexElement(element, index) {
+    if((index+1)%2 == 0) {
+        return true;
+    }
+}
+
+function isRepeat(element, index, array) {
+    for(var i=0; i<array.length; i++) {
+        if(element != array[i] && i!=index) {
+            return true;
+            break;
+        }
+        return false;
+    }
+}
+
 function single_element(arr) {
-  
+    return arr.filter(isEvenIndexElement).filter(isRepeat);
 }
